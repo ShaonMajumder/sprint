a1e0f954bb92e9ff63b88cca736619862f36ac4b
Shaon Majumder <smazoomder@gmail.com>
2021-12-05 13:01:13 +0600

    toastr progressbar added new task notification

--- resources/views/sprint.blade.php
+++ resources/views/sprint.blade.php
@@ -207,6 +207,23 @@
     }
 
     $(document).ready(function () {  
+      toastr.options = {
+        "closeButton": true,
+        "debug": false,
+        "newestOnTop": false,
+        "progressBar": true,
+        "positionClass": "toast-top-right",
+        "preventDuplicates": false,
+        "onclick": null,
+        "showDuration": "300",
+        "hideDuration": "1000",
+        "timeOut": "10000",
+        "extendedTimeOut": "10000",
+        "showEasing": "swing",
+        "hideEasing": "linear",
+        "showMethod": "fadeIn",
+        "hideMethod": "fadeOut"
+      };
 
       resize();
       $("#success-alert").hide();
